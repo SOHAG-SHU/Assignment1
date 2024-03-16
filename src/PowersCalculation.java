57805a0c5bf2344c00c6401b272fa1e67b1c0202
SOHAG-SHU <sohag209366@gmail.com>
2024-03-17 00:19:47 +0600

    Project01

---
+++ src/PowersCalculation.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class PowersCalculation {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        double number = scanner.nextDouble();
+        double square = number * number;
+        double cube = Math.pow(number, 3);
+        double fourthPower = Math.pow(number, 4);
+
+        System.out.println("Square: " + square);
+        System.out.println("Cube: " + cube);
+        System.out.println("Fourth power: " + fourthPower);
+        scanner.close();
+    }
+}
